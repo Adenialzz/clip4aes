df5d3ab052fdcbb8437455beb35177ae4d6c2e71
adenialzz <1160180309@qq.com>
2022-11-09 19:00:40 +0800

    fc weights as features for testing

--- zero_shot.py
+++ zero_shot.py
@@ -17,25 +17,30 @@ def parse_cfg():
     cfg = parser.parse_args()
     return cfg
 
+def load_features(task_name):
+    ckpt = torch.load(f'/media/song/iaa/{task_name}/weights/model_57.pth', map_location='cpu')
+    features = ckpt['state_dict']
+    return features['aes_fc.weight']
+
 def main(cfg):
     model, preprocess = clip.load(cfg.arch, device=cfg.device)
-    dataset = AVADataset(csv_file='/home/ps/JJ_Projects/FromSong/dsmAVA/csvFiles/val_mlsp.csv', root_dir='/ssd1t/song/Datasets/AVA/shortEdge256/all_images', transform=preprocess)
+    dataset = AVADataset(csv_file='/home/ps/JJ_Projects/FromSong/dsmAVA/csvFiles/test_mlsp.csv', root_dir='/ssd1t/song/Datasets/AVA/shortEdge256/all_images', transform=preprocess)
     dataloader = torch.utils.data.DataLoader(dataset, batch_size=cfg.batch_size, num_workers=10, pin_memory=True)
-    prompts = [ f'An image with an aesthetic score of {score} on a scale of 1-10.' for score in range(1, 11) ]
+    # prompts = [ f'An image with an aesthetic score of {score} on a scale of 1-10.' for score in range(1, 11) ]
     # prompts = [ 'An aesthetically negative picture.', 'An aesthetically positive image.' ]
-    assert len(prompts) == 2 or len(prompts) == 10  # for aesthetic binary classification and aesthetic distribution prediction
-    text_inputs = clip.tokenize(prompts).to(cfg.device)
-    text_features = model.encode_text(text_inputs)
-    text_features /= text_features.norm(dim=-1, keepdim=True)
+    # text_inputs = clip.tokenize(prompts).to(cfg.device)
+    # text_features = model.encode_text(text_inputs)
+    task_name = 'ViT-B-32_lr1e-2xbs128_noBias'
+    text_features = load_features(task_name).to(cfg.device).half()  # [512, 10]
+    # text_features /= text_features.norm(dim=0, keepdim=True)
 
     # metric loggers 
     _acc = AverageMeter()
-    if len(prompts) == 10:
-        _plcc_mean = AverageMeter()
-        _srcc_mean = AverageMeter()
-        _plcc_std = AverageMeter()
-        _srcc_std = AverageMeter()
-        _loss = AverageMeter()
+    _plcc_mean = AverageMeter()
+    _srcc_mean = AverageMeter()
+    _plcc_std = AverageMeter()
+    _srcc_std = AverageMeter()
+    _loss = AverageMeter()
 
 
     with torch.no_grad():
@@ -45,33 +50,21 @@ def main(cfg):
             bin_label = data["bin_cls"]
 
             image_features = model.encode_image(image)
-            image_features /= image_features.norm(dim=-1, keepdim=True)
+            # image_features /= image_features.norm(dim=-1, keepdim=True)
             output = (image_features @ text_features.T).softmax(dim=-1)
 
-            if len(prompts) == 10: 
-                acc, plcc_mean, srcc_mean, plcc_std, srcc_std = calc_aesthetic_metrics(output, label, bin_label)
-                loss = emd_loss(output, label)
+            acc, plcc_mean, srcc_mean, plcc_std, srcc_std = calc_aesthetic_metrics(output, label, bin_label)
+            loss = emd_loss(output, label)
 
-                _loss.update(loss.item())
-                _plcc_mean.update(plcc_mean)
-                _srcc_mean.update(srcc_mean)
-                _plcc_std.update(plcc_std)
-                _srcc_std.update(srcc_std)
-            else:
-                emd_class_pred = torch.zeros((image.shape[0]))
-                for idx in range(image.shape[0]):
-                    if output[idx][0] > 0.5: # negative
-                        emd_class_pred[idx] = 0.0
-                    else:
-                        emd_class_pred[idx] = 1.0
-                acc = metrics.accuracy_score(bin_label, emd_class_pred)
             _acc.update(acc)
+            _loss.update(loss.item())
+            _plcc_mean.update(plcc_mean)
+            _srcc_mean.update(srcc_mean)
+            _plcc_std.update(plcc_std)
+            _srcc_std.update(srcc_std)
 
-        if len(prompts) == 10:
-            print("emd_loss: {:.4f}, acc: {:.2f}, cc: {:.4f}, {:.4f}, {:.4f}, {:.4f}"
-            .format( _loss.avg, _acc.avg, _plcc_mean.avg, _srcc_mean.avg, _plcc_std.avg, _srcc_std.avg))
-        else:
-            print(f"acc: {_acc.avg:.2f}")
+        print("emd_loss: {:.4f}, acc: {:.2f}, cc: {:.4f}, {:.4f}, {:.4f}, {:.4f}"
+        .format( _loss.avg, _acc.avg, _plcc_mean.avg, _srcc_mean.avg, _plcc_std.avg, _srcc_std.avg))
 
 
 if __name__ == '__main__':
